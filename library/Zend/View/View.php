263d3055cd5e2466373efb3293444b48f74a3d96
Matthew Weier O'Phinney <matthew@zend.com>
2012-03-19 16:34:41 -0500

    Updated events-aware classes to implement EventManagerAware
    - Updated classes to implement EventManagerAware
    - Updated tests to use SharedEventManager instead of StaticEventManager

--- library/Zend/View/View.php
+++ library/Zend/View/View.php
@@ -22,6 +22,7 @@ namespace Zend\View;
 
 use Zend\EventManager\EventCollection,
     Zend\EventManager\EventManager,
+    Zend\EventManager\EventManagerAware,
     Zend\Stdlib\RequestDescription as Request,
     Zend\Stdlib\ResponseDescription as Response;
 
@@ -31,7 +32,7 @@ use Zend\EventManager\EventCollection,
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class View
+class View implements EventManagerAware
 {
     /**
      * @var EventCollection
