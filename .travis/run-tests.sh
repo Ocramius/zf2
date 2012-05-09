ef16abce34b2ca8588a5b237bdb88da842aad66b
Marco Pivetta <ocramius@gmail.com>
2012-05-09 22:31:16 +0200

    Making tests asynchronous to tweak performance

--- .travis/run-tests.sh
+++ .travis/run-tests.sh
@@ -7,13 +7,12 @@ result=0
 mkdir -p "$travisdir/log"
 cat "$travisdir/tested-components" | xargs -n 1 -L 1 -P 8 $travisdir/run-single-test.sh
 
-for testedComponent in "${testedComponents[@]}"
-    do
-        echo "$testedComponent:"
-        component=${testedComponent/\//-}
-        cat "$travisdir/log/$component-output"
-        componentResult=(`cat "$travisdir/log/$component-exitCode"`)
-        result=$(($result || $componentResult))
+for testedComponent in "${testedComponents[@]}" do
+    echo "$testedComponent:"
+    component=${testedComponent/\//-}
+    cat "$travisdir/log/$component-output"
+    componentResult=(`cat "$travisdir/log/$component-exitCode"`)
+    result=$(($result || $componentResult))
 done
 
 exit $result
